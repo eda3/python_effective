60316b4b216ac97a487bd991d1759c7415ea5696
eda3 <gerorin1010@gmail.com>
2019-03-24 18:49:23 +0900

    add: helper function

--- 008_helper_function.py
+++ 008_helper_function.py
@@ -17,3 +17,25 @@ opacity = my_values.get('opacity', [''])[0] or 0
 print(f'Red    :   {red}')
 print(f'Green  :   {green}')
 print(f'Opasity:   {opacity}')
+
+print('================')
+
+# User helper function as it isn't easy to read on one line
+
+
+def get_first_int(values, key, default=0):
+    found = values.get(key, [''])
+    if found[0]:
+        found = int(found[0])
+    else:
+        found = default
+    return found
+
+
+red = get_first_int(my_values, 'red')
+green = get_first_int(my_values, 'green')
+opacity = get_first_int(my_values, 'opacity')
+
+print(f'Red    :   {red}')
+print(f'Green  :   {green}')
+print(f'Opasity:   {opacity}')
