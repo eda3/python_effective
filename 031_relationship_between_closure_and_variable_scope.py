cefc43fc8cbadd7d3e3c8b300ec9bd2873ab1dd4
eda3 <gerorin1010@gmail.com>
2019-05-07 19:50:52 +0900

    add: use sort_priority()

--- 031_relationship_between_closure_and_variable_scope.py
+++ 031_relationship_between_closure_and_variable_scope.py
@@ -4,3 +4,9 @@ def sort_priority(values, group):
             return (0, x)
         return (1, x)
     values.sort(key=helper)
+
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {2, 3, 5, 7}
+sort_priority(numbers, group)
+print(numbers)
+# [2, 3, 5, 7, 1, 4, 6, 8]
