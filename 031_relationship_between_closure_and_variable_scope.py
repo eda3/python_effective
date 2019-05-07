0e83f5f7fb77fdc3de29d51d4001be26bfdac397
eda3 <gerorin1010@gmail.com>
2019-05-07 23:08:18 +0900

    add: sort_priority2

--- 031_relationship_between_closure_and_variable_scope.py
+++ 031_relationship_between_closure_and_variable_scope.py
@@ -12,3 +12,20 @@ group = {2, 3, 5, 7}
 sort_priority(numbers, group)
 print(numbers)
 # [2, 3, 5, 7, 1, 4, 6, 8]
+
+
+def sort_priority2(numbers, group):
+    found = False
+
+    def helper(x):
+        if x in group:
+            found = True  # Looks easy
+            return 0, x
+        return 1, 0
+    numbers.sort(key=helper)
+    return found
+
+
+found = sort_priority2(numbers, group)
+print('Found:', found)
+print(numbers)
