e77654e4d0b03773072c035591559c41b0d6e207
eda3 <gerorin1010@gmail.com>
2019-05-08 18:53:48 +0900

    fix: refactor

--- 031_relationship_between_closure_and_variable_scope.py
+++ 031_relationship_between_closure_and_variable_scope.py
@@ -10,7 +10,7 @@ def sort_priority(values, group):
 numbers = [8, 3, 1, 2, 5, 4, 7, 6]
 group = {2, 3, 5, 7}
 sort_priority(numbers, group)
-print(numbers)
+print(numbers, '\n')
 # [2, 3, 5, 7, 1, 4, 6, 8]
 
 
