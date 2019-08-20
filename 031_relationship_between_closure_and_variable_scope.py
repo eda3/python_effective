787e7e58456ebd413cb25b8ea6b960130d3ce339
eda3 <gerorin1010@gmail.com>
2019-08-20 21:37:51 +0900

    add: type hint

--- 031_relationship_between_closure_and_variable_scope.py
+++ 031_relationship_between_closure_and_variable_scope.py
@@ -1,4 +1,7 @@
-def sort_priority(values, group):
+from typing import List, Dict
+
+
+def sort_priority(values: List[int], group: Dict[int]) -> None:
     def helper(x):
         if x in group:
             return 0, x
@@ -7,25 +10,28 @@ def sort_priority(values, group):
     values.sort(key=helper)
 
 
-numbers = [8, 3, 1, 2, 5, 4, 7, 6]
-group = {2, 3, 5, 7}
+numbers: List[int] = [8, 3, 1, 2, 5, 4, 7, 6]
+group: Dict[int] = {2, 3, 5, 7}
 sort_priority(numbers, group)
 print(numbers, '\n')
+
+
 # [2, 3, 5, 7, 1, 4, 6, 8]
 
 
-def sort_priority2(numbers, group):
-    found = False
+def sort_priority2(numbers: List[int], group: Dict[int]) -> bool:
+    found: bool = False
 
     def helper(x):
         if x in group:
-            found = True  # Looks easy
+            found: bool = True  # Looks easy
             return 0, x
         return 1, 0
+
     numbers.sort(key=helper)
     return found
 
 
-found = sort_priority2(numbers, group)
+found: bool = sort_priority2(numbers, group)
 print('Found:', found)
 print(numbers)
