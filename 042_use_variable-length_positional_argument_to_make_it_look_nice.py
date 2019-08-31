6ae2e32e3f308f11e5c46c3259842a14a0b49a8c
eda3 <gerorin1010@gmail.com>
2019-08-31 16:23:37 +0900

    add: function log2()

--- 042_use_variable-length_positional_argument_to_make_it_look_nice.py
+++ 042_use_variable-length_positional_argument_to_make_it_look_nice.py
@@ -1,3 +1,4 @@
+# 可変長位置引数を使って、見た目をすっきりさせる
 from typing import List
 
 
@@ -9,9 +10,29 @@ def log(message: str, values: List[int]) -> None:
         print(f'{message}, {values_str}')
 
 
+def log2(message: str, *values: int) -> None:
+    if not values:
+        print(message)
+    else:
+        values_str: str = ', '.join((str(x) for x in values))
+        print(f'{message}, {values_str}')
+
+
 if __name__ == '__main__':
     # スター引数を使わない場合、いちいち空リストを渡さないといけない
     print('# function log()')
     log('My number are', [1, 2])
+    # これだとエラーになる
+    # log('Hi there')
     log('Hi there', [])
     print('---')
+
+    # 関数に可変長引数を用いることで、引数を省略することができる
+    print('# function log2()')
+    log2('My number are', 1, 2, 3)
+    log2('Hi there')  # ずっと良い！
+    print('---')
+
+    # 可変長引数にリストを渡すときはスター演算子を使う
+    favorites: List[int] = [7, 33, 99]
+    log2('Favorite colors', *favorites)
