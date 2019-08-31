c4075d22a65b6703ae3f80c00d18284da2883e63
eda3 <gerorin1010@gmail.com>
2019-08-31 16:07:55 +0900

    add: function log()

--- 042_use_variable-length_positional_argument_to_make_it_look_nice.py
+++ 042_use_variable-length_positional_argument_to_make_it_look_nice.py
@@ -1,2 +1,17 @@
+from typing import List
+
+
+def log(message: str, values: List[int]) -> None:
+    if not values:
+        print(message)
+    else:
+        values_str: str = ', '.join((str(x) for x in values))
+        print(f'{message}, {values_str}')
+
+
 if __name__ == '__main__':
-    pass
+    # スター引数を使わない場合、いちいち空リストを渡さないといけない
+    print('# function log()')
+    log('My number are', [1, 2])
+    log('Hi there', [])
+    print('---')
