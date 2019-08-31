dd0474d28597c804b86d7995493ea296484dbf51
eda3 <gerorin1010@gmail.com>
2019-08-31 11:53:17 +0900

    add: typing

--- 038_respect_certainty_when_using_iterators_on_arguments.py
+++ 038_respect_certainty_when_using_iterators_on_arguments.py
@@ -1,12 +1,18 @@
-def normalize(numbers: list) -> list:
-    total = sum(numbers)
-    result = []
+# 引数に対してイテレータを使うときには確実さを尊ぶ
+
+from typing import List
+
+
+def normalize(numbers: List[int]) -> List[float]:
+    total: int = sum(numbers)
+    result: List[float] = []
     for value in numbers:
         percent = 100 * value / total
         result.append(percent)
     return result
 
 
-visits = [15, 35, 80]
-percentages = normalize(visits)
+visits: List[int] = [15, 35, 80]
+percentages: List[float] = normalize(visits)
+
 print(percentages)
