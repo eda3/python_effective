0be51ffbdfdd25516d0389019c017aeeba1969c6
eda3 <gerorin1010@gmail.com>
2019-09-02 21:11:27 +0900

    add: function log()

--- 047_use_none_and_documentation-string_when_specifying_dynamic_default_arguments.py
+++ 047_use_none_and_documentation-string_when_specifying_dynamic_default_arguments.py
@@ -1,3 +1,18 @@
 # 動的なデフォルト引数を指定するときにはNoneとドキュメンテーション文字列を使う
 
+import time
+from datetime import datetime as dt
+
+
+def log(message: str, when: dt = dt.now()):
+    print(type(when))
+    print(f'{message}: {when}')
+
+
 if __name__ == '__main__':
+    # log()のdt.now()は関数定義の1度のみ評価されるため、
+    # 二回呼び出した際のタイムスタンプは同一になる
+    log('Hi there!')
+    time.sleep(1)
+    log('Hi again!')
+    print('---')
