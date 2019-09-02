4249c3b7807a80c83ba6d13607753d05c7ece6e2
eda3 <gerorin1010@gmail.com>
2019-09-02 21:25:23 +0900

    add: function log2()

--- 047_use_none_and_documentation-string_when_specifying_dynamic_default_arguments.py
+++ 047_use_none_and_documentation-string_when_specifying_dynamic_default_arguments.py
@@ -4,8 +4,21 @@ import time
 from datetime import datetime as dt
 
 
-def log(message: str, when: dt = dt.now()):
-    print(type(when))
+def log(message: str, when: dt = dt.now()) -> None:
+    print(f'{message}: {when}')
+
+
+def log2(message: str, when: dt = None) -> None:
+    """ タイムスタンプを入れつつログメッセージを出力
+
+    Args:
+        message: 表示するログメッセージ
+        when: メッセージが発生した日時。デフォルトは現在時刻
+
+    Returns:
+
+    """
+    when = dt.now() if when is None else when
     print(f'{message}: {when}')
 
 
@@ -15,4 +28,10 @@ if __name__ == '__main__':
     log('Hi there!')
     time.sleep(1)
     log('Hi again!')
+    print('---')
+
+    # デフォルト引数をNoneにしておき、1度目と2度目の呼び出しで挙動を変える
+    log2('Hi there!')
+    time.sleep(1)
+    log2('Hi again!')
     print('---')
