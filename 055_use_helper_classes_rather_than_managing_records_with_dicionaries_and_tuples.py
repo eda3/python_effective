eb7d1af3c9370c4106f1101306bd2d4c5dd37227
eda3 <gerorin1010@gmail.com>
2019-09-13 20:22:47 +0900

    add: class BySubjectGradebook

--- 055_use_helper_classes_rather_than_managing_records_with_dicionaries_and_tuples.py
+++ 055_use_helper_classes_rather_than_managing_records_with_dicionaries_and_tuples.py
@@ -62,6 +62,61 @@ class SimpleGradebook(object):
         return self._grades
 
 
+class BySubjectGradebook(object):
+    """科目別成績記録クラス
+    学生集団の成績を科目別に記録する
+    まだギリギリ、DictとListで管理出来るが、型ヒントを見ると複雑になっているのが分かる
+    """
+
+    def __init__(self) -> None:
+        self._grades: Dict[str, Dict[str, List[int]]] = {}
+
+    def add_student(self, name: str) -> None:
+        """学生名追加メソッド
+
+        Args:
+            name (str): 学生名
+
+        Returns:
+            None
+        """
+        self._grades[name]: Dict[str, List[int]] = {}
+
+    def report_grade(self, name: str, subject: str, grade: int) -> None:
+        """科目別スコア追加メソッド
+
+        Args:
+            name (str): スコア追加対象の学生名
+            subject (str): 科目名
+            grade (int): 追加するスコア
+
+        Returns:
+            None
+        """
+        by_subject: Dict[str, List[int]] = self._grades[name]
+        grade_list = by_subject.setdefault(subject, [])
+        grade_list.append(grade)
+
+    def average_grade(self, name: str) -> float:
+        """平均スコア取得メソッド
+
+        Args:
+            name (str): 学生名
+
+        Returns:
+            float: 平均スコア
+
+
+        """
+        by_subject: Dict[str, List[int]] = self._grades[name]
+        total: int = 0
+        count: int = 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+
 if __name__ == '__main__':
     # 追加する学生の名前を設定
     student_name: str = 'Isaac Newton'
@@ -73,4 +128,17 @@ if __name__ == '__main__':
     book.report_grade(student_name, 0)
     print(book.average_grade(student_name))
     # >>> 50.0
+    print('---')
+
+    # 追加する学生の名前を設定
+    student_name: str = 'Albert Einstein'
+    book2: BySubjectGradebook = BySubjectGradebook()
+    book2.add_student(student_name)
+    book2.add_student('eda')
+    book2.report_grade(student_name, 'Math', 75)
+    book2.report_grade(student_name, 'Math', 65)
+    book2.report_grade(student_name, 'Gym', 90)
+    book2.report_grade(student_name, 'Gym', 95)
+    print(book2.average_grade(student_name))
+    # >>> 81.25
     print('---')
