a0bb69beae1bbdebd3a07d90ab464d0b132c3a07
eda3 <gerorin1010@gmail.com>
2019-09-17 20:28:51 +0900

    add: sort by name length

--- 061_use_functions_instead_of_classes_for_simple_interfaces.py
+++ 061_use_functions_instead_of_classes_for_simple_interfaces.py
@@ -1,3 +1,10 @@
 # 単純なインターフェースにはクラスの代わりに関数を使う
+from typing import List
 
 if __name__ == '__main__':
+    # List型のsortメソッドにのオプションとしてkey引数を取り、
+    # keyフックとしてlambda式を与えてソートする
+    names: List[str] = ['Socrates', 'Archimedes', 'Plato', 'Aristotle']
+    names.sort(key=lambda x: len(x))
+    print(names)
+    # >>> ['Plato', 'Socrates', 'Archimedes', 'Aristotle']
