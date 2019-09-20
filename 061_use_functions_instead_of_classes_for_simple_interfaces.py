bdfd0a5bf09c074b51625a22dac110c288b79f64
eda3 <gerorin1010@gmail.com>
2019-09-21 08:02:19 +0900

    add: function log_missing()

--- 061_use_functions_instead_of_classes_for_simple_interfaces.py
+++ 061_use_functions_instead_of_classes_for_simple_interfaces.py
@@ -1,7 +1,13 @@
 # 単純なインターフェースにはクラスの代わりに関数を使う
 from typing import List
 
-if __name__ == '__main__':
+
+def log_missing() -> int:
+    print("Key added")
+    return 0
+
+
+if __name__ == "__main__":
     # List型のsortメソッドにのオプションとしてkey引数を取り、
     # keyフックとしてlambda式を与えてソートする
     names: List[str] = ['Socrates', 'Archimedes', 'Plato', 'Aristotle']
