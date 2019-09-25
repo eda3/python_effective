81d0889eacb7f479f32e1ac67c5028bc2c3f3489
eda3 <gerorin1010@gmail.com>
2019-09-25 19:23:24 +0900

    add: function use_log_missing

--- 061_use_functions_instead_of_classes_for_simple_interfaces.py
+++ 061_use_functions_instead_of_classes_for_simple_interfaces.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import Dict, Tuple
+
 # 単純なインターフェースにはクラスの代わりに関数を使う
 from typing import List
 
@@ -7,6 +10,26 @@ def log_missing() -> int:
     return 0
 
 
+def use_log_missing():
+    current: Dict[str, int] = {"green": 12, "blue": 3}
+    increments: List[Tuple[str, int]] = [("red", 5), ("blue", 17), ("orange", 9)]
+    result = defaultdict(log_missing, current)
+    print("Before:", dict(result))
+
+    # currentのdictについて、incrementsから一つひとつキーチェックする
+    # キーにマッチするものがあればそれの値に修正
+    # キーにマッチしないものがなければ(defaultdictならば)、
+    # 新規にキーと値を追加する（そして"Key added"を出力"
+    for key, amount in increments:  # type: str, int
+        result[key] += amount
+    print("After: ", dict(result))
+
+    # >> Before: {'green': 12, 'blue': 3}
+    # >> Key added
+    # >> Key added
+    # >> After:  {'green': 12, 'blue': 20, 'red': 5, 'orange': 9}
+
+
 if __name__ == "__main__":
     # List型のsortメソッドにのオプションとしてkey引数を取り、
     # keyフックとしてlambda式を与えてソートする
@@ -14,3 +37,8 @@ if __name__ == "__main__":
     names.sort(key=lambda x: len(x))
     print(names)
     # >>> ['Plato', 'Socrates', 'Archimedes', 'Aristotle']
+
+    print("---")
+
+    # 関数log_missingを利用する
+    use_log_missing()
