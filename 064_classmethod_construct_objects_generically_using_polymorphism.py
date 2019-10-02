140de06b64c918a5b303a065754914c1cde01a3b
eda3 <gerorin1010@gmail.com>
2019-10-03 06:46:56 +0900

    add: class InputData, class PathInputData

--- 064_classmethod_construct_objects_generically_using_polymorphism.py
+++ 064_classmethod_construct_objects_generically_using_polymorphism.py
@@ -1,5 +1,23 @@
 # @classmethodポリモルフィズムを使ってオブジェクトをジェネリックに構築する
 
 
+class InputData(object):
+    """ 入力データを表す共通クラス
+
+    """
+
+    def read(self):
+        raise NotImplementedError
+
+
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        return open(self.path).read()
+
+
 if __name__ == "__main__":
     pass
