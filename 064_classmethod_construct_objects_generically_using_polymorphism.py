1fc26aaaec8ad638673f3e0ff6b66971e1bb337b
eda3 <gerorin1010@gmail.com>
2019-10-19 11:48:43 +0900

    add: generate_inputs(), create_workers(), execute(), reduce_map

--- 064_classmethod_construct_objects_generically_using_polymorphism.py
+++ 064_classmethod_construct_objects_generically_using_polymorphism.py
@@ -40,5 +40,40 @@ class LineCountWorker(Worker):
         self.result += other.result
 
 
+def generate_inputs(data_dir):
+    """
+    ディレクトリの内容をリストしてそこに含まれる各ファイルに対する
+    PathInputDataを作成する
+    """
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for threads in threads:
+        threads.start()
+    for threads in threads:
+        threads.join()
+
+    first, rest = workers[0], workers[1:]
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+
+def reduce_map(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+
 if __name__ == "__main__":
     pass
