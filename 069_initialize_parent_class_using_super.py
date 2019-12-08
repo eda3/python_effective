339258bec6fe9c60092550e2ab3e7a52b7b803f8
eda3 <gerorin1010@gmail.com>
2019-12-08 16:19:34 +0900

    add: class PlusFile

--- 069_initialize_parent_class_using_super.py
+++ 069_initialize_parent_class_using_super.py
@@ -5,9 +5,19 @@ class TimesTwo(object):
         self.value *= 2
 
 
+class PlusFive(object):
+    value = 0
+
+    def __init__(self):
+        self.value += 5
+
+
 def main():
     tw = TimesTwo()
     print("tw.values", tw.value)
+
+    pf = PlusFive()
+    print("pf.value", pf.value)
 
 
 if __name__ == "__main__":
