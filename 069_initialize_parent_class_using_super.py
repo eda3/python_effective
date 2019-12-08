ce26052520cefbc7d9d86948c4657d097e4807f4
eda3 <gerorin1010@gmail.com>
2019-12-08 16:16:28 +0900

    add: class TimesTwo

--- 069_initialize_parent_class_using_super.py
+++ 069_initialize_parent_class_using_super.py
@@ -1,5 +1,13 @@
+class TimesTwo(object):
+    value = 0
+
+    def __init__(self):
+        self.value *= 2
+
+
 def main():
-    pass
+    tw = TimesTwo()
+    print("tw.values", tw.value)
 
 
 if __name__ == "__main__":
