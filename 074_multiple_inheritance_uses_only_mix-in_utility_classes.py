2e2e9089745ac5e0bc18ac7f4dd25e0f3bd10676
eda3 <gerorin1010@gmail.com>
2019-12-30 11:32:12 +0900

    add: class ToDictMixin

--- 074_multiple_inheritance_uses_only_mix-in_utility_classes.py
+++ 074_multiple_inheritance_uses_only_mix-in_utility_classes.py
@@ -1,6 +1,20 @@
 # 多重継承はmix-inユーティリティクラスだけに使う
 
 
+class ToDictMixin(object):
+    """Pythonのオプジェクトをメモリ内の表現からシリアライズできる辞書表現に変換する
+
+    """
+
+    def to_dict(self):
+        """辞書型に変換
+
+        Returns:
+
+        """
+        return self._traverse_dict(self.__dict__)
+
+
 def main():
     pass
 
